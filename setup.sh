9a0a4b8407991d0226635b6dd05b36e5f832d2ae
Ben Nuttall <ben@bennuttall.com>
2015-06-12 14:50:07 +0100

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,4 @@
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install vim ipython ipython3 python-dev python3-dev python-pip python3-pip ntpdate -y
+wget http://bennuttall.com/vimrc -O /home/pi/.vimrc
