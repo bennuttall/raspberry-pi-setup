0ddf38148ed2d90389e980b670f32a2c453dfd52
Ben Nuttall <ben@bennuttall.com>
2015-09-24 23:09:35 +0100

    Add symlink to make root's vimrc match pi's

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 hostname -I
 wget --no-check-certificate http://bennuttall.com/vimrc -O /home/pi/.vimrc
+sudo ln -s /home/pi/.vimrc /root/.vimrc
 echo "alias ll='ls -alF'" >> .bash_aliases
 mkdir -p /home/pi/.ssh
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDg+VUw2Y+GozBgr7maX+jQhjF2gvwGKXVatGywYVbVQJ6qOsQJozLfgdl+nY55/B/JrjtjsSa03iqpF8+fXvYovv8f9M/v2juhZqGibUQAdwwd4EftPepUIWA0c/gsz4/XhjtUdIkOQ9M9BD29zD0dWsTGiKlQqFVFAk0jy4sQbh2yIvppS5atnUsct5XQFaRIhbaUi//0pe8DUPYds+zefLQTTB/+/qnHlb7iycux3A0sxXjYguFE1XezeidVpFLJIFbG+XSMM43E8u+zGLnDubLsrWNObSlZzXlwPvqq9G7OyhiE/r1gG8Dmaq9yF8wrj7UdZKMeyMnsl8xHrkq7 ben@marcy" >> .ssh/authorized_keys
