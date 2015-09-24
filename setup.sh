61e54f348b6c655318bbb89573a2793c42d69cae
Ben Nuttall <ben@bennuttall.com>
2015-09-24 23:07:42 +0100

    Add gunter ssh key

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ wget --no-check-certificate http://bennuttall.com/vimrc -O /home/pi/.vimrc
 echo "alias ll='ls -alF'" >> .bash_aliases
 mkdir -p /home/pi/.ssh
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDg+VUw2Y+GozBgr7maX+jQhjF2gvwGKXVatGywYVbVQJ6qOsQJozLfgdl+nY55/B/JrjtjsSa03iqpF8+fXvYovv8f9M/v2juhZqGibUQAdwwd4EftPepUIWA0c/gsz4/XhjtUdIkOQ9M9BD29zD0dWsTGiKlQqFVFAk0jy4sQbh2yIvppS5atnUsct5XQFaRIhbaUi//0pe8DUPYds+zefLQTTB/+/qnHlb7iycux3A0sxXjYguFE1XezeidVpFLJIFbG+XSMM43E8u+zGLnDubLsrWNObSlZzXlwPvqq9G7OyhiE/r1gG8Dmaq9yF8wrj7UdZKMeyMnsl8xHrkq7 ben@marcy" >> .ssh/authorized_keys
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDVoOKB417bwbchulJ3D3p19vOS2hh9N9P1gpGMjeFTyO+RifAL/OE+E1CePeszYd3hPLQXiFbMWLUDC44jfY9XspsSdgzAJEI9JIzfMG2+C7LNSwbHIpXHZNhV6vW8N2OH4OdWVwZ0oRCElL9VtmvU96rtaoBay5h9e2KdVZCjjTrk9HEZEmEtN8f8f0gQKhFdyft8tb736KrnAQzmnWd6bujwadSYFyoz0M8uwrDhpNRYE5ZQVNT+P/Jr/NRfjzMz7jCm/7JaKx2cznKBJ6gUq1jBiW5/UYfDt++cfrqcUkiTBBIhKt1qXJuP6DrBEEUNRm0jb0VAwA26qQcpPuJ9 ben@gunter" >> .ssh/authorized_keys
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install vim ipython ipython3 python-dev python3-dev python-pip python3-pip ntpdate -y
